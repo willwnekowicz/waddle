e8217b742c0beafa289b26c0ad533862f85742f7
Vishal Reddy <vreddy18@gmail.com>
2014-03-29 13:18:52 -0400

    adds twitter omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,32 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+
+  def self.from_omniauth(auth)
+  	where(auth.slice(:provider, :uid)).first_or_create do |user|
+    	user.provider = auth.provider
+    	user.uid = auth.uid
+    	user.username = auth.info.nickname
+  	end
+end
+
+def self.new_with_session(params, session)
+  if session["devise.user_attributes"]
+    new(session["devise.user_attributes"], without_protection: true) do |user|
+      user.attributes = params
+      user.valid?
+    end
+  else
+    super
+  end    
+endgit
+
+	private
+
+	def user_params 
+ 		params.require(:user).permit(:email, :password, :password_confirmation, :remember_me, :username)
+	end
+  
+  validates_presence_of :username
+  validates_uniqueness_of :username
 end
