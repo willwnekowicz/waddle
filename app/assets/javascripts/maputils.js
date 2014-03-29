66b52679b1c7d402d565bf501715d0c82ee30f37
Faber Fedor <faberfedor@gmail.com>
2014-03-29 19:50:54 -0400

    first code using AJAX against the backend

--- app/assets/javascripts/maputils.js
+++ app/assets/javascripts/maputils.js
@@ -1,39 +1,76 @@
-var map = L.mapbox.map('map', 'examples.map-20v6611k').setView([39.12367, -76.81229], 9);
+    
+    var map = L.mapbox.map('map', 'waddleuser.hlae5cf4')
+               .setView([0, 0], 0);
+   
 
-var geoJson = {
-    type: 'FeatureCollection',
-    features: [{
-        type: 'Feature',
-        properties: {
-            title: 'Washington, D.C.',
-            'marker-color': '#f00',
-            'marker-size': 'large',
-            url: 'http://en.wikipedia.org/wiki/Washington,_D.C.'
+    var geoJson = {
+        type: 'FeatureCollection',
+        features: [{
+            type: 'Feature',
+            properties: {
+                title: 'Washington, D.C.',
+                'marker-color': '#f00',
+                'marker-size': 'large',
+                url: 'http://en.wikipedia.org/wiki/Washington,_D.C.'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-77.03201, 38.90065]
+            }
         },
-        geometry: {
-            type: 'Point',
-            coordinates: [-77.03201, 38.90065]
-        }
-    },
-    {
-        type: 'Feature',
-        properties: {
-            title: 'Baltimore, MD',
-            'marker-color': '#f00',
-            'marker-size': 'large',
-            url: 'http://en.wikipedia.org/wiki/Baltimore'
-        },
-        geometry: {
-            type: 'Point',
-            coordinates: [-76.60767, 39.28755]
-        }
-    }]
-};
+        {
+            type: 'Feature',
+            properties: {
+                title: 'Baltimore, MD',
+                'marker-color': '#f00',
+                'marker-size': 'large',
+                url: 'http://en.wikipedia.org/wiki/Baltimore'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-76.60767, 39.28755]
+            }
+        }]
+    };
+   
+    /* 
+    var newCoords = geoJson.features[0].geometry.coordinates;
+    var newMarker = geoJson.features[0].properties.title;
 
-// Pass features and a custom factory function to the map
-map.featureLayer.setGeoJSON(geoJson);
-map.featureLayer.on('click', function(e) {
-    e.layer.unbindPopup();
-    window.open(e.layer.feature.properties.url);
-});
+    // Pass features and a custom factory function to the map
+    map.featureLayer.setGeoJSON(geoJson);
+    map.featureLayer.on('click', function(e) {
+        e.layer.unbindPopup();
+        window.open(e.layer.feature.properties.url);
+    });
 
+   var geocoder = L.mapbox.geocoder('waddleuser.hlae5cf4', map);
+   geocoder.query(newMarker, showMap);
+
+   function showMap(err, data) {
+     map.fitBounds(data.lbounds);
+   }
+   */
+    var dummy = 1;
+
+var mapData = function() {
+
+    var map = L.mapbox.map('map', 'waddleuser.hlae5cf4')
+               .setView([0, 0], 0);
+
+    // id comes from where?
+    var url = 'user/' + id + '.json';
+    var geoJson = Object;
+    geoJson.type = 'FeatureCollection';
+    
+    $.getJSON( dataType: 'json',
+               url     : url,
+               success : function (data, textStatus, jqXHR) {
+                   geoJson.features = data;
+                   map.featureLayer.setGeoJSON(geoJson);
+                   
+               }
+
+ 
+   }
+ 
