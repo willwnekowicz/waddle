ccaaf436ebeacd34a9e3f86dced8872c926080de
Faber Fedor <faberfedor@gmail.com>
2014-03-29 14:31:09 -0400

    working example of map with markers

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Waddle::Application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get 'map' => 'home#map'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
