cc1053fb1161dae4abd0f0096d2f8ca94137b414
William Wnekowicz <william.wnekowicz@gmail.com>
2014-03-29 09:34:15 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Waddle::Application.config.secret_key_base = '9c7a034b289271a62ec0f37f41a560c1a22285c756e83f34af980496e66cb97cdadd9645026ec19a789a6216e20f2e387d2eb99a5a394b8a406890f8bc04fa09'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Waddle::Application.config.session_store :cookie_store, key: '_waddle_session'
